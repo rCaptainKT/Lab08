04080bfc410b7f20a00fd977003a71c8e44b57c0
rCaptainKT <144297314+rCaptainKT@users.noreply.github.com>
2025-10-29 18:13:23 -0600

    added test cases for deleteCity() and countCities()

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -10,4 +10,36 @@ public class CustomListTest {
         list.addCity(calgary);
         assertTrue(list.hasCity(calgary));
     }
+
+    @Test
+    void testDelete() {
+        CustomList list = new CustomList();
+        City city1 = new City("Regina", "Saskatchewan");
+        City city2 = new City("Calgary", "Alberta");
+        list.addCity(city1);
+        list.addCity(city2);
+        assertEquals(3, list.countCities());
+
+        list.deleteCity(city2);
+        assertEquals(2, list.countCities());
+
+        list.deleteCity(city1);
+        assertEquals(1, list.countCities());
+    }
+
+    @Test
+    void testDeleteException() {
+        CustomList list = new CustomList();
+        City city = new City("Yellowknife", "Northwest Territories");
+        assertThrows(IllegalArgumentException.class, () -> {
+            list.deleteCity(city);
+        });
+    }
+
+    @Test
+    void testCountCities() {
+        CustomList list = new CustomList();
+        assertEquals(1, list.countCities());
+        assertNotEquals(2, list.countCities());
+    }
 }
